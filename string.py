1c7a515158f284d86b3499df1be105929e8169a4
DevLeeYG <hi33741@naver.com>
2022-05-25 13:26:54 +0900

    [파이썬] 문자열 공부중

---
+++ string.py
@@ -0,0 +1,16 @@
+def add(a,b):
+  return a+b
+
+
+add(3,5)
+
+food = '''asdkfl;aks;dfkal';sdf'''
+print(food)
+
+a = 'you need python'
+
+print(len(a))
+
+b = a[0:3]
+
+print(b)
--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-a = 100
-b = 200
+# a = 100
+# b = 200
 
-print(a+b)
+# print(a+b)
+
+a = 2
+
+# if a > 1:
+#   print('a is greater than 1')
+# else:
+#   print(1)
+
+# for b in [1,2,3]:
+#   print(b)
+
+i = 0 
+while i < 3:
+  i = i + 1
+  print(i)
