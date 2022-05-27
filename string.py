18967ebbc6ff4782917db5f66a59704f539a5be0
DevLeeYG <hi33741@naver.com>
2022-05-27 13:29:09 +0900

    05-27 문자열

--- string.py
+++ string.py
@@ -1,16 +1,50 @@
-def add(a,b):
-  return a+b
+# def add(a,b):
+#   return a+b
 
 
-add(3,5)
+# add(3,5)
 
-food = '''asdkfl;aks;dfkal';sdf'''
-print(food)
+# food = '''asdkfl;aks;dfkal';sdf'''
+# print(food)
 
-a = 'you need python'
+# a = 'you need python'
 
-print(len(a))
+# print(len(a))
 
-b = a[0:3]
+# b = a[0:3]
 
-print(b)
+# print(b)
+
+# ab = 'python\'s faviorite food is perl'
+
+
+# abc = 'you need python'
+
+# print(len(abc))
+
+
+# life = "life is too shot, you need Python"
+
+# print(life[0:5])
+
+
+
+# a = "pithon"
+# print(a[:1] + 'y' + a[2:])
+
+
+# print("I eat %d apples." %3)
+# # %d 는 문자열 포맷 코드라고 부른다
+
+# #문자열을 넣기 위해선 %s 숫자를 넣기위해선 %d
+
+# number =3
+
+# print("I eat %d apples" %number)
+
+
+number = 10
+
+day = "three"
+
+print("I ate %d apples. so I was sick %s days." %(number,day))
