ffbee59ce5c300aadeea6d3a0ce9a5ed854ec4a4
DevLeeYG <hi33741@naver.com>
2022-06-15 14:14:47 +0900

    3

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 dic = {'name':'pay'}
 
 print(dic['name'])
@@ -7,3 +8,10 @@ dic['age'] = 19
 del dic['name']
 
 print(dic)
+
+sport = {1:'피겨스케이팅',2:'야구',3:'축구'}
+
+bc = sport.keys()
+
+print(bc)
+
