06571918af816c0cfada29529ea566b4170c7746
DevLeeYG <hi33741@naver.com>
2022-06-24 13:30:17 +0900

    2

--- dictionary.py
+++ dictionary.py
@@ -13,5 +13,17 @@ sport = {1:'피겨스케이팅',2:'야구',3:'축구'}
 
 bc = sport.keys()
 
-print(bc)
 
+
+h = {'name':'pey','phone':'01123','birth':'1118'}
+dd = h.keys()
+
+print(dd)
+
+for k in h.keys():#k 객체안에서 key를 반복으로 넣는다
+    print(k)
+
+item = h.items()
+print(item)
+
+print(h.get('name'))
---
+++ tuple.py
@@ -0,0 +1,18 @@
+s1 = set([1,2,3,4,5,6,])
+s2 = set([4,5,6,7,8,9,])
+
+print(s1|s2)
+
+print(s1.union(s2))
+
+print(s1.difference(s2))
+
+print(s2.difference(s1))
+
+s1.add(10)
+
+print(s1)
+
+print(s1&s2)
+
+print(s1.intersection(s2))
